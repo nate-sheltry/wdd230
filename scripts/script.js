75fb0abb560d3920639a9b2b1db9c05b49930503
Nathaniel Dunham <dun19007@byui.edu>
2023-04-29 12:30:28 -0700

    Added additional CSS and fixed Javascript errors.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 
-const date = new Date()
+const date = new Date();
 const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 
 const __textYear = document.querySelector("#footer_year")
 const __modifiedDate = document.querySelector("#last_modified_date")
 
 __textYear.textContent = date.getFullYear();
-__modifiedDate.textContent = `${String(date.getMonth()).padStart(2, '0')}/${String(date.getDay()).padStart(2, '0')}/${date.getFullYear()} ${time}`;
+__modifiedDate.textContent = `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${date.getFullYear()} ${time}`;
