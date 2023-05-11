52fe3b7dbf946e032f2a07525f4a1a6fcc23b896
Nathaniel Dunham <dun19007@byui.edu>
2023-05-10 18:57:04 -0700

    finalized the css for the base stylesheet file.

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -5,3 +5,15 @@ function toggleMenu(){
     __menu[0].classList.toggle("responsive");
     
 }
+
+// select the DOM elements to manipulate (we will output to these)
+const datefield = document.querySelector("time");
+// for european/family history format with day first.
+
+// derive the current date using a date object
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	new Date()
+);
+// "full" or long, medium, short options ... try them
+
+datefield.textContent = fulldate;
