5ba4f4619e17b28ae599e49740872cabed33c5e7
Nathaniel Dunham <dun19007@byui.edu>
2023-05-16 14:05:39 -0700

    finished the book of mormon list DOM manipulation.

---
+++ lesson5/scripts/script.js
@@ -0,0 +1,33 @@
+const __button = document.querySelector("button");
+const __input = document.querySelector("#favchap");
+const __list = document.querySelector("#list");
+
+let item;
+let button;
+
+__button.addEventListener("click", () => {
+    if(/[a-z]/i.test(__input.value) == false || /[0-9]/.test(__input.value) == false){
+        alert("Not a valid Chapter format")
+        __input.focus();
+        return
+    }
+
+    item = document.createElement("li")
+    button = document.createElement("button")
+
+    button.textContent = "❌";
+    item.textContent = __input.value;
+
+    item.appendChild(button);
+    
+    button.addEventListener("click", (e) => {
+        removeEventListener("click", e.target)
+        e.target.parentElement.remove();
+        __input.focus();
+    });
+
+    __list.appendChild(item);
+    __input.value = '';
+    __input.focus();
+
+});
